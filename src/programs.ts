ce2098bb273f56262c2355ad4bd0ca543d62baea
Matt <matthew@adler.id.au>
2026-05-09 16:39:27 +1000

    Flesh out programs.ts

--- src/programs.ts
+++ src/programs.ts
@@ -1,8 +1,37 @@
-export const programs = {
-    nuke: 'nuke.exe',
-    brutessh: 'brutessh.exe',
-    ftpcrack: 'ftpcrack.exe',
-    relaysmtp: 'relaysmtp.exe',
-    httpworm: 'httpworm.exe',
-    sqlinject: 'sqlinject.exe',
-};
+import { NS } from '@ns';
+
+interface Program {
+    exe: string;
+    nsFunc: (host: string) => boolean;
+}
+
+interface AllPrograms {
+    [key: string]: Program;
+}
+
+export const getPrograms: (ns: NS) => AllPrograms = (ns: NS) => ({
+    nuke: {
+        exe: 'NUKE.exe',
+        nsFunc: ns.nuke,
+    },
+    brutessh: {
+        exe: 'BruteSSH.exe',
+        nsFunc: ns.brutessh,
+    },
+    ftpcrack: {
+        exe: 'FTPCrack.exe',
+        nsFunc: ns.ftpcrack,
+    },
+    relaysmtp: {
+        exe: 'relaySMTP.exe',
+        nsFunc: ns.relaysmtp,
+    },
+    httpworm: {
+        exe: 'HTTPWorm.exe',
+        nsFunc: ns.httpworm,
+    },
+    sqlinject: {
+        exe: 'SQLInject.exe',
+        nsFunc: ns.sqlinject,
+    },
+});
