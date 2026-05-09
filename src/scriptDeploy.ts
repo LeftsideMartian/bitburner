08838dfa38036bc6ae5ae93408b60b0ab7123baf
Matt <matthew@adler.id.au>
2026-05-09 16:39:39 +1000

    Script: Create scriptDeploy

---
+++ src/scriptDeploy.ts
@@ -0,0 +1,25 @@
+import { NS } from '@ns';
+import { getExternalServersList } from './utils';
+import { updateServersFile } from './spider';
+
+export async function main(ns: NS) {
+    const scriptName = 'earlyHack.js';
+
+    updateServersFile(ns);
+    const servers: string[] = getExternalServersList(ns);
+
+    ns.tprint(`Deploying to the following servers: ${servers.join(', ')}`);
+
+    servers.forEach(server => {
+        if (!ns.fileExists(scriptName, server)) {
+            ns.scp(scriptName, server);
+        }
+
+        while (
+            ns.getServerMaxRam(server) - ns.getServerUsedRam(server) >=
+            ns.getScriptRam(scriptName)
+        ) {
+            ns.exec(scriptName, server);
+        }
+    });
+}
