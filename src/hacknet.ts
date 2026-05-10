0878857ef80df444b75775823c5c9b76ed6f003c
Matt <matthew@adler.id.au>
2026-05-10 12:25:16 +1000

    Script: Create hacknet script to buy and upgrade hacknet nodes

---
+++ src/hacknet.ts
@@ -0,0 +1,35 @@
+import { NS } from '@ns';
+import { homeNode } from './constants';
+
+export async function main(ns: NS) {
+    await manageHacknet(ns);
+}
+
+export async function manageHacknet(ns: NS) {
+    const hacknet = ns.hacknet;
+
+    const hacknetPurchaseThreshold = 100;
+    const sleepDuration = 1000; // in MS
+
+    let numOfOwnedNodes = hacknet.numNodes();
+
+    while (true) {
+        const currentMoney = ns.getServerMoneyAvailable(homeNode);
+        const purchaseThreshold = currentMoney / hacknetPurchaseThreshold;
+
+        // Try to purchase new node
+        if (hacknet.getPurchaseNodeCost() <= purchaseThreshold) {
+            hacknet.purchaseNode();
+            numOfOwnedNodes++;
+        }
+
+        // Try to upgrade all nodes
+        for (let i = 0; i < numOfOwnedNodes; i++) {
+            if (hacknet.getLevelUpgradeCost(i) <= purchaseThreshold) hacknet.upgradeLevel(i);
+            if (hacknet.getRamUpgradeCost(i) <= purchaseThreshold) hacknet.upgradeRam(i);
+            if (hacknet.getCoreUpgradeCost(i) <= purchaseThreshold) hacknet.upgradeCore(i);
+        }
+
+        await ns.sleep(sleepDuration);
+    }
+}
