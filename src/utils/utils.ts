892b9e145ac289381903733797c1e69d136a1941
Matt <matthew@adler.id.au>
2026-05-10 14:55:48 +1000

    New utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,11 @@
 import { NS, Server } from '@ns';
 import { homeNode, serversFileName } from './constants';
 
+type NSMethods = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [K in keyof NS]: NS[K] extends (...args: any[]) => any ? K : never;
+}[keyof NS];
+
 export const getServersList = (ns: NS): string[] => {
     const serversFile = ns.read(serversFileName);
 
@@ -10,7 +15,11 @@ export const getServersList = (ns: NS): string[] => {
 
     return serversFile.split('\n');
 };
+
 export const getExternalServersList = (ns: NS): string[] =>
     getServersList(ns).filter(server => server !== homeNode);
 export const getExternalServersListTyped = (ns: NS): Server[] =>
     getExternalServersList(ns).map(node => ns.getServer(node) as Server); // Filtering out Darknet servers - these WILL NOT be included in return
+
+export const disableLogging = (ns: NS, nsMethods: NSMethods[]) =>
+    nsMethods.forEach(method => ns.disableLog(method));
