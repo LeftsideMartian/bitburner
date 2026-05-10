e549ed819691b9141185253035adbfbde8796b5f
Matt <matthew@adler.id.au>
2026-05-10 14:55:40 +1000

    New constants

--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const homeNode = 'home';
-export const serversFileName = 'servers.txt';
---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+export const homeNode = 'home';
+
+// Directories
+const scriptDirectory = 'scripts/';
+const txtFileDirectory = 'txt/';
+
+// Txt files
+export const serversFileName = txtFileDirectory + 'servers.txt';
+
+// Scripts
+export const spiderScriptName = scriptDirectory + 'spider.js';
+export const hacknetScriptName = scriptDirectory + 'hacknet.js';
